410c3307df0bfa2c4fef458d74a367ceff80e918
hernandezalvarezsatwit <hernandezalvarezs@wit.edu>
2022-03-21 09:00:31 -0400

    Added comments

--- app/src/main/java/edu/wit/mobileapp/myinternalsotrage/MainActivity.java
+++ app/src/main/java/edu/wit/mobileapp/myinternalsotrage/MainActivity.java
@@ -10,9 +10,11 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Notice no additional permissions are needed
     private String filename = "myFile";
     private TextView text;
     private Button readDataBtn;
@@ -23,39 +25,55 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String string = "Hello World Android!!\n";
+        //Data that will be written
+        String string = "Sample written data\n";
         FileOutputStream outputStream;
 
-        context = getApplicationContext();
+        context = this;
         text = findViewById(R.id.text);
         readDataBtn = findViewById(R.id.ReadBtn);
 
         try{
+            //pass file name
             outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            //Write as bytes
             outputStream.write(string.getBytes());
+            //Close right away
             outputStream.close();
+            //Confirm everything went well
             text.setText("Wrote data to file "+filename+ " successfully.");
+            //Update UI
             text.invalidate();
         }catch (Exception e){
             Log.v("myApp","Error: "+e);
         }
 
+        //Button action to read data and show it on screen
         readDataBtn.setOnClickListener(v ->{
             FileInputStream fis;
             InputStreamReader isr;
             BufferedReader bufferedReader;
 
             try{
+                //Get the file
                 fis = context.openFileInput(filename);
+                //Get the stream
                 isr = new InputStreamReader(fis);
+                //Use buffer reader to read stream
                 bufferedReader = new BufferedReader(isr);
+                //To hold the data
                 StringBuilder sb = new StringBuilder();
 
+                //Buffer read contents and append to sb
                 String line;
                 while((line = bufferedReader.readLine())!= null){
                     sb.append(line);
                 }
+
+                //Set to text to show in screen
                 text.setText(sb.toString());
+
+                //Update UI
                 text.invalidate();
             }catch(Exception e){
                 Log.v("myApp", "Error: "+e);
